e0b57ff1d910fe5dab77dd0eafd223098d1a82d9
zPikaa <dhaab9653@gmail.com>
2023-07-31 22:27:32 +0200

    Update 1.7.3

--- src/main/java/it/pika/pockethorses/PocketHorses.java
+++ src/main/java/it/pika/pockethorses/PocketHorses.java
@@ -104,7 +104,7 @@ public final class PocketHorses extends JavaPlugin {
     private static boolean modelEngineEnabled = false;
 
 
-    public static final String VERSION = "1.7.2";
+    public static final String VERSION = "1.7.3";
 
     @Override
     public void onLoad() {
